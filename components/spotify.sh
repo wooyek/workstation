9ebcc41ec12d7eda53b9dc625e8423537f83a2d4
Janusz Skonieczny <git.js@bravelabs.pl>
2019-12-05 15:32:25 +0100

    network tools

--- components/spotify.sh
+++ components/spotify.sh
@@ -3,14 +3,9 @@
 echo "----> Spotify"
 echo "----> https://www.spotify.com/pl/download/linux/"
 
-# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-
-# 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
-# 3. Update list of available packages
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-
-# 4. Install Spotify
 sudo apt-get install spotify-client
+
+
